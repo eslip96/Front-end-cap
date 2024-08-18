a86063a70c082a6b54b528aabd805e8d076df45d
enoka silipa <eslip96@gmail.com>
2024-08-18 15:47:38 -0600

    added things

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,8 @@ import { NavLink } from "react-router-dom";
 export default function NavBar() {
   return (
     <div className="nav-bar-wrapper">
-      <h1>Nav Bar LOGO</h1>
+      <h1>JAKE & IANS CLOTHING CO</h1>
+
       <div className="nav-links-contianer">
         <NavLink to="/home">Home</NavLink>
         <NavLink to="/products">Products</NavLink>
