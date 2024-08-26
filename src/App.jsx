8f617dcc3aa57f317039ae816625c4289e78cbec
enoka silipa <eslip96@gmail.com>
2024-08-26 12:38:11 -0600

    fixed the about page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Footer from "./components/Footer";
 import ShoppingCart from "./components/ShoppingCart";
 import Contact from "./components/Contact";
 import About from "./components/About";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
@@ -17,12 +18,13 @@ function App() {
         <NavBar />
         <Switch>
           <div className="default-page-wrapper">
-            <Route path="/" component={HomePage} />
+            <Route path="/home" component={HomePage} />
             <Route path="/products" component={ProductsPage} />
             <Route path="/product/:id" component={ProductPage} />
             <Route path="/shopping-cart" component={ShoppingCart} />
             <Route path="/contact" component={Contact} />
             <Route path="/about" component={About} />
+            <Redirect from="/" to="/home" />
           </div>
         </Switch>
         <Footer />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,8 @@ export default function About() {
     <div className="about-page-wrapper">
       <div className="about-container">
         <h1>About</h1>
+        <p>write stuff here</p>
+        <h1>IMAGE HERE</h1>
       </div>
     </div>
   );
