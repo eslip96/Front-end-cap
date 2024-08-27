fba71ed4d03de0b3bf2964814664167458f60920
enoka silipa <eslip96@gmail.com>
2024-08-27 12:04:34 -0600

    fixed capstone

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,22 @@ export default function About() {
   return (
     <div className="about-page-wrapper">
       <div className="about-container">
-        <h1>About</h1>
-        <p>write stuff here</p>
-        <h1>IMAGE HERE</h1>
+        <div className="about-me-wrapper">
+          <h1 className="about-title">About Me</h1>
+          <p className="about-text">
+            Greetings! My name is Enoka Patrick Silipa and this is my magnum
+            opus, the crowning achievement I've concocted up these past two
+            weeks. I've taken the front-end class before but I ended up taking a
+            long break right after and almost lost a good chunk of my React.js
+            knowledge. So here I am taking it again! Which is fine. Devpipeline
+            is helping me become a developer, and I feel more confident every
+            day! The hardest part for me—and I can only speak for myself because
+            I'm not you *insert your name here* whoever you are reading this—has
+            been spending a lot of time researching context and its purposes and
+            integrating that with your components. Luckily, we are in my
+            favorite languages of front-end: JavaScript, HTML, CSS.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,39 @@
+import React from "react";
+
 export default function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("message sent!");
+    document.getElementById("contact-form").reset();
+  };
+
   return (
     <div className="contact-page-wrapper">
       <div className="about-me-container">
-        <h1>Contact</h1>
-        <h1>image here</h1>
-        <h1>paragraph here</h1>
+        <h1>Contact Us!</h1>
+        <form id="contact-form" onSubmit={handleSubmit}>
+          <div className="contact-form-piece">
+            <label htmlFor="first-name">First Name</label>
+            <input type="text" id="first-name" name="first-name" required />
+          </div>
+
+          <div className="contact-form-piece">
+            <label htmlFor="last-name">Last Name</label>
+            <input type="text" id="last-name" name="last-name" required />
+          </div>
+
+          <div className="contact-form-piece">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+          <div className="contact-form-piece">
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" required></textarea>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
