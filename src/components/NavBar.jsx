ea313816e7f82cbeed6fab7e30bad6dc380b43a4
enoka silipa <eslip96@gmail.com>
2024-08-28 23:14:58 -0600

    made front-end cap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="nav-bar-wrapper">
       <h1>JAKE & IANS CLOTHING CO</h1>
 
-      <button className="hamburger-button">|||</button>
+      <button
+        className="hamburger-button"
+        onClick={() => setMenuOpen((prevState) => !prevState)}
+      >
+        MENU
+      </button>
 
-      <div className="nav-links-container">
+      <div className={menuOpen ? "menu-open links-wrapper" : "links-wrapper"}>
         <NavLink className="nav-link-box" to="/home">
           Home
         </NavLink>
-        <NavLink className="nav-link-box" to="/products">
+        <NavLink className="nav-link-box " to="/products">
           Products
         </NavLink>
         <NavLink className="nav-link-box" to="/contact">
